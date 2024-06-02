3a860470d5873652dbb302bf0e542ccf652f1e12
小傅哥 <184172133@qq.com>
2024-06-03 07:28:08 +0800

    feat：《大营销平台系统设计实现》 - 营销服务 第26节：积分领域调额服务

--- big-market-app/src/test/java/cn/bugstack/test/domain/credit/CreditAdjustServiceTest.java
+++ big-market-app/src/test/java/cn/bugstack/test/domain/credit/CreditAdjustServiceTest.java
@@ -33,7 +33,7 @@ public class CreditAdjustServiceTest {
         tradeEntity.setTradeName(TradeNameVO.REBATE);
         tradeEntity.setTradeType(TradeTypeVO.FORWARD);
         tradeEntity.setAmount(new BigDecimal("10.19"));
-        tradeEntity.setOutBusinessNo("100009909911");
+        tradeEntity.setOutBusinessNo("12406039900002");
         creditAdjustService.createOrder(tradeEntity);
     }
 
